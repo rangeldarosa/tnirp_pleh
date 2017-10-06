3fc9bca17d9c017ef7130ceaaf464feea258322e
Raphael Santos Custódio <raphael5178@hotmail.com>
2017-10-05 22:03:42 -0300

    - ADICIONADO VIEW DE PROFESSOR

---
+++ application/view/_templates/professores/listProfessores.tpl.php
@@ -0,0 +1,30 @@
+<div class="listProfessoresArea">
+  <div class="table-responsive">
+    <table class="table table-responsive table-striped">
+      <thead>
+            <th>Cod</th>
+            <th>Nome</th>
+            <th>Privado</th>
+            <th><span class="glyphicon glyphicon-pencil"></span></th>
+            <th><span class="glyphicon glyphicon-remove"></span></th>
+      </thead>
+      <tbody>
+        <?php
+        foreach($professores as $professor) {
+        ?>
+        <tr class="<?php echo $professor->ESTADO == 0 ? 'danger' : ''?>">
+            <td><?php echo $professor->CD_PROFESSOR;?></td>
+            <td><?php echo $professor->NOME;?></td>
+            <td><?php echo $professor->PRIVADO == '1' ? 'Sim' : 'Não';?></td>
+            <td><a title="Editar Professor" href="<?php echo URL; ?>professor/editarProfessor/<?php echo $professor->CD_PROFESSOR;?>"><span class="glyphicon glyphicon-pencil"></span></a></td>
+            <td><a title="Bloquear Professor" href="<?php echo URL; ?>professor/bloquearProfessor/<?php echo $professor->CD_PROFESSOR;?>">
+              <span class="glyphicon glyphicon-<?php echo $professor->ESTADO == '1' ? 'remove' : 'ok';?>"></span>
+            </a></td>
+        </tr>
+        <?php
+        }
+        ?>
+      </tbody>
+    </table>
+  </div>
+</div>
--- application/view/professor/index.php
+++ application/view/professor/index.php
@@ -1,27 +1,25 @@
 <div class="container">
-<h2 class="text-center">Listagem de professores</h2><br>
-<div class="panel panel-default">
-  <div class="panel-heading">
-    <h3 class="panel-title">Cadastro de Professores</h3>
+  <h2 class="text-center">Listagem de professores</h2><br>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h3 class="panel-title">Cadastro de Professores</h3>
+    </div>
+    <div class="panel-body">
+      <?php
+      include APP . 'view/_templates/professores/cadProfessores.tpl.php';
+      ?>
+    </div>
   </div>
-  <div class="panel-body">
-    <?php
-    include APP . 'view/_templates/professores/cadProfessores.tpl.php';
-    ?>
-  </div>
-</div>
 
-<!-- TODO  FALTA IMPLEMENTAR AS LISTAS -->
-<div class="panel panel-default">
-  <div class="panel-heading">
-    <h3 class="panel-title">Lista de Professores</h3>
+  <!-- TODO  FALTA IMPLEMENTAR AS LISTAS -->
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h3 class="panel-title">Lista de Professores</h3>
+    </div>
+    <div class="panel-body">
+      <?php
+        include APP . 'view/_templates/professores/listProfessores.tpl.php';
+      ?>
+    </div>
   </div>
-  <div class="panel-body">
-    <?php
-    foreach($professores as $professor) {
-        echo $professor->NOME . "<br />";
-    }
-    ?>
-  </div>
-</div>
 </div>
